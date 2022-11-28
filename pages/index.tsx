ce70b403996156b70e55753d24e2495c99214830
twotimesgi <luigibardellagerbi@mail.com>
2022-11-28 18:35:22 +0100

    Fix viewport

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,7 @@ export default function Home() {
       <Head>
         <title>dChat</title>
         <meta name="description" content="Simple decentralized chat" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
